b639e8b90d9dc5d8ce638f3e409a259a202ea567
Alxay <alxay2137@gmail.com>
2026-02-02 12:20:17 +0100

    Refactor piece movement and frame handling for improved game logic and performance

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ print(gameBoard)
 
 def generatePiece():
     piece = np.zeros((20,10),dtype=int)
-    piece[0][1] = 1
     piece[0][2] = 1
     piece[0][3] = 1
-    piece[1][2] = 1
+    piece[0][4] = 1
+    piece[1][3] = 1
     return piece
    
 
@@ -32,51 +32,141 @@ def drawBoard(gameBoard,piece=np.zeros((20,10),dtype=int)):
     ))
     #[[i j],[i j]]
     for i,j in blocks:
-        print(f"Klocek {j}, {i}")
+        # print(f"Klocek {j}, {i}")
         rect = pygame.Rect(j*35,i*35,35,35)
         pygame.draw.rect(screen,"purple",rect)
         
 
-def move(gameBoard,piece):
+def frame(gameBoard,piece):
     gameBoardBlocks = np.argwhere(gameBoard == 1)
     pieceBlocks = np.argwhere(piece == 1)
     gameBoardBlocks = set(map(tuple, gameBoardBlocks))
     piece2 = np.zeros((20,10),dtype=int)
     for i,j in pieceBlocks:
-        if(i >= 19 or (i+1,j) in gameBoardBlocks):
-            print("Block under the piece !!")
+        if(i >= 19 or gameBoard[i+1][j] == 1 ):
+            print("Block under the piece !!\n")
+            # Return gameBoard with piece added
+            gameBoard = np.maximum(gameBoard,piece)
+            print("Final Game Board:")
+            print(gameBoard)
+            return generatePiece(),gameBoard
             
         else:
             piece2[i+1][j] = 1
-            piece[i][j] = 0
+            # piece[i][j] = 0
         
-    return piece2
+    return piece2,gameBoard
 
+def maxCords(blocks):
+    minX = 100
+    maxX = 0
+    minY = 100
+    maxY = 0
+    for i,j in blocks:
+        if i > maxX:
+            maxX = i
+        if i < minX:
+            minX = i
+        if j > maxY:
+            maxY = j
+        if j < minY:
+            minY = j
+    return minX,maxX,minY,maxY
+def move(gameBoard,piece,direction):
+    print("ruch")
+    pieceBlocks = np.argwhere(piece == 1)
+    print(pieceBlocks)
+    minX,maxX,minY,maxY = maxCords(pieceBlocks)
+    piece2 = np.zeros((20,10),dtype=int)
+    if direction == 1:
+        if maxY >= 9:
+            return piece
+        for i,j in pieceBlocks:
+            piece2[i][j+1] = 1
+    if direction == 2:
+        if minY <= 0:
+            return piece
+        for i,j in pieceBlocks:
+            piece2[i][j-1] = 1
+    if direction == 3:
+        centerX,centerY = (minX+maxX)//2,(minY+maxY)//2
+        print(f"minX: {minX}, maxX: {maxX}, minY: {minY}, maxY: {maxY}")
+        print(f"centerX: {centerX}, centerY: {centerY}")
+        fragment = piece[centerX-1:centerX+2, centerY-1:centerY+2] # kwadrat 3x3 do obrotu
+        print(fragment)
+        rotated = np.rot90(fragment, -1)  # -1 = 90° w prawo
+        print(rotated)
+        piece2[centerX-1:centerX+2, centerY-1:centerY+2] = rotated
+        if np.any((gameBoard==1)&(piece2==1)):
+            print("Kolizja po obrocie!")
+            return piece
+        
+        
+    if direction == 4:
+        minX = 100
+        maxX = 0
+        minY = 100
+        maxY = 0
+        for i,j in pieceBlocks:
+            if i > maxX:
+                maxX = i
+            if i < minX:
+                minX = i
+            if j > maxY:
+                maxY = j
+            if j < minY:
+                minY = j
+        centerX,centerY = (minX+maxX)//2,(minY+maxY)//2
+        print(f"minX: {minX}, maxX: {maxX}, minY: {minY}, maxY: {maxY}")
+        print(f"centerX: {centerX}, centerY: {centerY}")
+        fragment = piece[centerX-1:centerX+2, centerY-1:centerY+2] # kwadrat 3x3 do obrotu
+        print(fragment)
+        rotated = np.rot90(fragment, 1)  # -1 = 90° w prawo
+        print(rotated)
+        piece2[centerX-1:centerX+2, centerY-1:centerY+2] = rotated
+    return piece2
 
 
 piece = generatePiece()
 gameBoard[19][0] = 1
 gameBoard[19][1] = 1
 gameBoard[10][2] = 1
+# Tworzymy własne ID zdarzenia
+TICK_EVENT = pygame.USEREVENT + 1
+
+# Ustaw timer, który wysyła TICK_EVENT co 1000 ms (1 sekunda)
+pygame.time.set_timer(TICK_EVENT, 1000)
+
 while running:
     #game loop
     screen.fill("black")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == TICK_EVENT:
+            print("Tick event received!")
+            piece, gameBoard = frame(gameBoard,piece)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_0]:
         screen.fill("green")
+    if keys[pygame.K_d]:
+        piece = move(gameBoard,piece,1)
+    if keys[pygame.K_a]:
+        piece = move(gameBoard,piece,2)
+    if keys[pygame.K_w]:
+        piece = move(gameBoard,piece,3)    
+    if keys[pygame.K_s]:
+        piece = move(gameBoard,piece,4)      
 
+
+    # print(piece)
+    # print("-----")
+    # print(gameBoard)
     drawGrid()
-    piece = move(gameBoard,piece)
-    print(piece)
-    print("-----")
-    print(gameBoard)
     drawBoard(gameBoard,piece)
     pygame.display.flip()
-    clock.tick(1)
+    clock.tick(10)
 
 
 
