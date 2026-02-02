ece0122b7462288e7c00fd9d3fea225893da260d
Alxay <alxay2137@gmail.com>
2026-02-02 14:04:21 +0100

    Enhance collision detection and movement logic in game mechanics

--- main.py
+++ main.py
@@ -57,6 +57,11 @@ def frame(gameBoard,piece):
         
     return piece2,gameBoard
 
+def checkColision(gameBoard,piece):
+    if np.any((gameBoard==1)&(piece==1)):
+        return True
+    return False
+
 def maxCords(blocks):
     minX = 100
     maxX = 0
@@ -81,15 +86,19 @@ def move(gameBoard,piece,direction):
     piece2 = np.zeros((20,10),dtype=int)
     if direction == 1:
         if maxY >= 9:
-            return piece
+            return piece,gameBoard
         for i,j in pieceBlocks:
             piece2[i][j+1] = 1
+        if checkColision(gameBoard,piece2):
+            return piece,gameBoard
         
     if direction == 2:
         if minY <= 0:
-            return piece
+            return piece,gameBoard
         for i,j in pieceBlocks:
             piece2[i][j-1] = 1
+        if checkColision(gameBoard,piece2):
+            return piece,gameBoard
     if direction == 3:
         centerX,centerY = (minX+maxX)//2,(minY+maxY)//2
         print(f"minX: {minX}, maxX: {maxX}, minY: {minY}, maxY: {maxY}")
@@ -103,8 +112,8 @@ def move(gameBoard,piece,direction):
         print(fragment)
         rotated = np.rot90(fragment, -1)  # -1 = 90° w prawo
         print(rotated)
-        if minY <= 0:
-            return piece
+        if minY <= 0 or maxY >= 9 or minX <= 0 or maxX >= 19:
+            return piece,gameBoard
         piece2[x1:x2, y1:y2] = rotated
 
         # try:
@@ -114,9 +123,9 @@ def move(gameBoard,piece,direction):
         #     return piece
         if np.any((gameBoard==1)&(piece2==1)):
             print("Kolizja po obrocie!")
-            return piece
+            return piece,gameBoard
         
-    return piece2
+    return piece2,gameBoard
 
 
 piece = generatePiece()
@@ -143,20 +152,11 @@ while running:
     if keys[pygame.K_0]:
         screen.fill("green")
     if keys[pygame.K_d]:
-        piece = move(gameBoard,piece,1)
-        heckcolision = np.any((gameBoard==1)&(piece==1))
-        if heckcolision:
-            piece,gameBoard = frame(gameBoard,piece)
+        piece,gameBoard = move(gameBoard,piece,1)
     if keys[pygame.K_a]:
-        piece = move(gameBoard,piece,2)
-        heckcolision = np.any((gameBoard==1)&(piece==1))
-        if heckcolision:
-            piece,gameBoard = frame(gameBoard,piece)
+        piece,gameBoard = move(gameBoard,piece,2)
     if keys[pygame.K_w]:
-        piece = move(gameBoard,piece,3)    
-        heckcolision = np.any((gameBoard==1)&(piece==1))
-        if heckcolision:
-            piece,gameBoard = frame(gameBoard,piece)
+        piece,gameBoard = move(gameBoard,piece,3)    
     if keys[pygame.K_s]:   
         piece, gameBoard = frame(gameBoard,piece)
 
